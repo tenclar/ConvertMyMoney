c455ad8ff7b82545e5d90e29759a8d30af67dc84
Tenclar Valus <tenclarvalus@gmail.com>
2019-04-26 10:39:10 -0500

    capturar informação de webservice para cotação do dolar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const convert = require('./lib/convert')
-
+const apiBCB = require('./lib/bcb')
 const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
@@ -13,8 +13,13 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.get('/', (req, res) =>{
-    res.render('home')
+app.get('/', async(req, res) =>{
+
+    const cotacao = await apiBCB.getCotacao()
+
+    res.render('home', {
+        cotacao
+    })
 })
 app.get('/cotacao', (req, res) =>{
     const {cotacao, quantidade } = req.query
